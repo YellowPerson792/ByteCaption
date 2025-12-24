239bbae427446a6656da390f6d8d4cb78cfc05cc
ypfree <yellow_person@outlook.com>
2025-12-24 15:48:57 +0800

    sync

--- tools/run_batch_corruption_eval.py
+++ tools/run_batch_corruption_eval.py
@@ -6,7 +6,7 @@ PureT/experiments/ByteCaption_XE
 Example:
 python tools/run_batch_corruption_eval.py \
 --models PureT/experiments/ByteCaption_XE_qwen \
---corrupt-types rbsl \
+--corrupt-types rbbf \
 --corrupt-levels S3 S4 S5 \
 --save-captions 500 \
 --test-samples 0
